710a602d9957a7323e065c19fc451b2dfdc71997
Yumi Patton <yumipatton@gmail.com>
2021-05-04 13:58:10 +1000

    initial attempt

---
+++ copy_property_list.py
@@ -0,0 +1,40 @@
+import psycopg2
+from config import config
+from io import StringIO
+import csv
+
+def copy_property_list(property_list):
+    # """ insert multiple vendors into the vendors table  """
+    # sql = "INSERT INTO property (valuer_general_id, address, saleprice, saledate) values(%s,%s,%s,%s)"
+    conn = None
+
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        # cur.executemany(sql, property_list)
+       	csv_file_like_object = StringIO()
+        writer = csv.writer(csv_file_like_object, delimiter=';')
+        writer.writerows(property_list)
+        csv_file_like_object.seek(0)
+        cur.copy_from(csv_file_like_object, "property", sep=";", columns=(
+            'valuer_general_id', 'address', 'saleprice', 'saledate'))
+        csv_file_like_object.close()
+        csv_file_like_object.truncate(0)
+        csv_file_like_object.seek(0)
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print("the process completed")
+
+
--- readfiles.py
+++ readfiles.py
@@ -1,6 +1,8 @@
 import os
 from read_dat import read_dat
-from insert_property_list import insert_property_list
+# from insert_property_list import insert_property_list
+from copy_property_list import copy_property_list
+
 
 # def loop_directory(directory: str):
 #     for filename in os.listdir(directory):
@@ -18,7 +20,11 @@ def loop_directory(directory: str):
             # print(filename)
 			value_list = read_dat(file_directory, value_list)
 
-	insert_property_list(value_list)
+	# copy_from		
+
+	copy_property_list(value_list)
+	## executemany
+	# insert_property_list(value_list)
 	#print(value_list)
 
 if __name__ == '__main__':
